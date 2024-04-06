b29c8112736c452f0813d4a79780f6fe9ccb2f31
vladimir.chikaidze <vladimir.chikaidze@latoken.com>
2024-04-06 13:42:12 +0400

    Add project files.

---
+++ Plugins/MathPlugin.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace SemanticKernelLatokenTask.Plugins
+{
+    public class MathPlugin
+    {
+        [KernelFunction]
+        [Description("Sums two integers and returns the result as a string.")]
+        public int Sum(
+            // Description в параметрах не обязателен
+            [Description("The first integer to be added.")] int x,
+            [Description("The second integer to be added.")] int y)
+        {
+            int sum = x + y;
+            Console.WriteLine($"Sum {x} + {y} = " + sum);
+            return x + y;
+        }
+
+        [KernelFunction]
+        [Description("Multiplies two integers and returns the result.")]
+        public int Multiply(
+            // Description в параметрах не обязателен
+            [Description("The first integer to be multiplied.")] int x,
+            [Description("The second integer to be multiplied.")] int y)
+        {
+            int result = x * y;
+            Console.WriteLine($"Multiplying {x} * {y} = " + result);
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using SemanticKernelLatokenTask.Plugins;
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddOpenAIChatCompletion(
+      "gpt-4-1106-preview",
+         Environment.GetEnvironmentVariable("openai-api-key"));
+
+builder.Plugins.AddFromType<MathPlugin>();
+
+var kernel = builder.Build();
+
+IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory chatMessages = new ChatHistory();
+
+while (true)
+{
+    System.Console.Write("User > ");
+
+    chatMessages.AddUserMessage(Console.ReadLine()!);
+
+    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(
+        chatMessages,
+        executionSettings: new OpenAIPromptExecutionSettings()
+        {
+            ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+            Temperature = 0
+        },
+        kernel: kernel);
+
+    string fullMessage = "";
+
+    await foreach (var content in result)
+    {
+        if (content.Role.HasValue)
+            System.Console.Write("\nAssistant > ");
+        
+        System.Console.Write(content.Content);
+        fullMessage += content.Content;
+    }
+
+    System.Console.WriteLine();
+    chatMessages.AddAssistantMessage(fullMessage);
+}
